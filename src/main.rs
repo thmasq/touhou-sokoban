185171b47ad33dcdd0927d05c357f8d941affc40
tH0m4Ssxsqrd <thomasqsa@gmail.com>
2022-04-30 19:56:33 -0300

    Changes the first map

--- src/main.rs
+++ src/main.rs
@@ -58,7 +58,7 @@ impl<'a> System<'a> for RenderingSystem<'a> {
     fn run(&mut self, data: Self::SystemData) {
         let (positions, renderables) = data;
 
-        // Clearing the screen (this gives us the background colour)
+        // Clearing the screen (this gives us the backround colour)
         graphics::clear(self.context, graphics::Color::new(0.95, 0.95, 0.95, 1.0));
 
         // Get all the renderables with their positions and sort by the position z
@@ -179,32 +179,61 @@ pub fn create_player(world: &mut World, position: Position) {
 
 // Initialize the level
 pub fn initialize_level(world: &mut World) {
-    create_player(
-        world,
-        Position {
-            x: 0,
-            y: 0,
-            z: 0, // we will get the z from the factory functions
-        },
-    );
-    create_wall(
-        world,
-        Position {
-            x: 1,
-            y: 0,
-            z: 0, // we will get the z from the factory functions
-        },
-    );
-    create_box(
-        world,
-        Position {
-            x: 2,
-            y: 0,
-            z: 0, // we will get the z from the factory functions
-        },
-    );
+    const MAP: &str = "
+    N N W W W W W W
+    W W W . . . . W
+    W . . . B . . W
+    W . . . . . . W 
+    W . P . . . . W
+    W . . . . . . W
+    W . . S . . . W
+    W . . . . . . W
+    W W W W W W W W
+    ";
+
+    load_map(world, MAP.to_string());
 }
 
+pub fn load_map(world: &mut World, map_string: String) {
+    // read all lines
+    let rows: Vec<&str> = map_string.trim().split('\n').map(|x| x.trim()).collect();
+
+    for (y, row) in rows.iter().enumerate() {
+        let columns: Vec<&str> = row.split(' ').collect();
+
+        for (x, column) in columns.iter().enumerate() {
+            // Create the position at which to create something on the map
+            let position = Position {
+                x: x as u8,
+                y: y as u8,
+                z: 0, // we will get the z from the factory functions
+            };
+
+            // Figure out what object we should create
+            match *column {
+                "." => create_floor(world, position),
+                "W" => {
+                    create_floor(world, position);
+                    create_wall(world, position);
+                }
+                "P" => {
+                    create_floor(world, position);
+                    create_player(world, position);
+                }
+                "B" => {
+                    create_floor(world, position);
+                    create_box(world, position);
+                }
+                "S" => {
+                    create_floor(world, position);
+                    create_box_spot(world, position);
+                }
+                "N" => (),
+                c => panic!("unrecognized map item {}", c),
+            }
+        }
+    }
+}
 pub fn main() -> GameResult {
     let mut world = World::new();
     register_components(&mut world);
