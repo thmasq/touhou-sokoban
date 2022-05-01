294976fd002286a556cbfbe91089c4bc31e271bc
tH0m4Ssxsqrd <thomasqsa@gmail.com>
2022-04-30 21:14:55 -0300

    Separates the source code into modules

---
+++ src/resources.rs
@@ -0,0 +1,13 @@
+// resources.rs
+use ggez::event::KeyCode;
+use specs::World;
+
+// Resources
+#[derive(Default)]
+pub struct InputQueue {
+    pub keys_pressed: Vec<KeyCode>,
+}
+
+pub fn register_resources(world: &mut World) {
+    world.insert(InputQueue::default())
+}
