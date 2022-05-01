9ff21f13175772cd09e9a9430bddde2beddeae70
tH0m4Ssxsqrd <thomasqsa@gmail.com>
2022-04-30 21:30:08 -0300

    Implements gameplay counters and game_state check

--- src/systems/rendering_system.rs
+++ src/systems/rendering_system.rs
@@ -1,14 +1,34 @@
 
 
+
 use crate::components::*;
+use crate::resources::*;
 use crate::constants::TILE_WIDTH;
 
-use ggez::{Context, graphics::{self, DrawParam, Image}};
-use specs::{Join, ReadStorage, System};
+use ggez::{Context, graphics::{self, DrawParam, Image, Color}};
+use specs::{Join, ReadStorage, System, Read};
 use glam::Vec2;
 
 pub struct RenderingSystem<'a> {
     pub context: &'a mut Context,
+}
+
+impl RenderingSystem<'_> {
+    pub fn draw_text(&mut self, text_string: &str, x: f32, y: f32) {
+        let text = graphics::Text::new(text_string);
+        let destination = Vec2::new(x, y);
+        let color = Some(Color::new(0.0, 0.0, 0.0, 1.0));
+        let dimensions = Vec2::new(0.0, 20.0);
+
+        graphics::queue_text(self.context, &text, dimensions, color);
+        graphics::draw_queued_text(
+            self.context,
+            graphics::DrawParam::new().dest(destination),
+            None,
+            graphics::FilterMode::Linear,
+        )
+        .expect("expected drawing queued text");
+    }
 }
 
 // System implementation
@@ -16,10 +36,10 @@ pub struct RenderingSystem<'a> {
 // 'a is a lifetime annotation which prevents the value from being dropped after it it out of scope. It is necessary in this case
 impl<'a> System<'a> for RenderingSystem<'a> {
     // Data
-    type SystemData = (ReadStorage<'a, Position>, ReadStorage<'a, Renderable>);
+    type SystemData = (Read<'a, Gameplay>, ReadStorage<'a, Position>, ReadStorage<'a, Renderable>);
 
     fn run(&mut self, data: Self::SystemData) {
-        let (positions, renderables) = data;
+        let (gameplay, positions, renderables) = data;
 
         // Clearing the screen (this gives us the backround colour)
         graphics::clear(self.context, graphics::Color::new(0.95, 0.95, 0.95, 1.0));
@@ -41,6 +61,10 @@ impl<'a> System<'a> for RenderingSystem<'a> {
             let draw_params = DrawParam::new().dest(Vec2::new(x, y));
             graphics::draw(self.context, &image, draw_params).expect("expected render");
         }
+
+        // Render any text
+        self.draw_text(&gameplay.state.to_string(), 525.0, 80.0);
+        self.draw_text(&gameplay.moves_count.to_string(), 525.0, 100.0);
 
         // Finally, present the context, this will actually display everything
         // on the screen.
